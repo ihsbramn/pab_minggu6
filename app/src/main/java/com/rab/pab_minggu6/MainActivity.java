89af12dab144819146461dcddc67d7eb8ed7853d
ihsbramn <abdurahmanihsan25@gmail.com>
2021-11-03 20:36:21 +0700

    initial

---
+++ app/src/main/java/com/rab/pab_minggu6/MainActivity.java
@@ -0,0 +1,21 @@
+package com.rab.pab_minggu6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClicked(View view) {
+        Intent intent = new Intent(this, NewsActivity.class);
+        startActivity(intent);
+    }
+}
